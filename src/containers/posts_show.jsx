ba466cb102279447c0355c2c15522170bc3083f7
Adam Sinning <asinning25@gmail.com>
2019-04-18 15:28:35 -0300

    posts show completed with new action

--- src/containers/posts_show.jsx
+++ src/containers/posts_show.jsx
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchPost } from '../actions/index';
 
 class PostsShow extends Component {
+    componentDidMount() {
+      if (!this.props.post) {
+        this.props.fetchPost(this.props.match.params.id);
+      }
+    }
+
    render() {
     if (!this.props.post) {
       return <p>Loading...</p>;
@@ -21,10 +29,13 @@ class PostsShow extends Component {
     );
   }
 }
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchPost }, dispatch);
+}
 
 function mapStateToProps(state, ownProps) {
   const idFromUrl = parseInt(ownProps.match.params.id, 10); // From URL
   const post = state.posts.find(p => p.id === idFromUrl);
   return { post };
 }
-export default connect(mapStateToProps)(PostsShow);
+export default connect(mapStateToProps, mapDispatchToProps)(PostsShow);
